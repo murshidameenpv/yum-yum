8809d4418ba24775bb7158b0ac1a3de935115056
murshidameen <murshidmonzpv@gmail.com>
2024-01-22 05:01:46 +0530

    pagination logic completed in menu

--- client/src/pages/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Cards from '../../components/Cards';
-
+import { FaFilter } from 'react-icons/fa'
 function Menu() {
   const [menu, setMenu] = useState([]);
   const [filteredItems, setFilteredItems] = useState([])
   const [selectedCategory, setSelectedCategory] = useState("all")
-  console.log(selectedCategory,"ppppppppppppppp");
+  // console.log(selectedCategory,"ppppppppppppppp");
   const [sortOption, setSortOption] = useState("default")
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage] = useState(8)
   //loading data
   useEffect(() => {
     // hardcoded data
@@ -29,11 +32,13 @@ function Menu() {
     const filtered = category === "all" ? menu : menu.filter((item) => item.category === category);
     setFilteredItems(filtered);
     setSelectedCategory(category)
+    setCurrentPage(1)
   }
   //Show all menu
   const showAllMenu = () => {
     setFilteredItems(menu)
     setSelectedCategory("all")
+    setCurrentPage(1)
   }
   //Sorting based on A -Z,Z-A,pricing
   const handleSorting = (option) => {
@@ -58,7 +63,13 @@ function Menu() {
         break;
     }
     setFilteredItems(sortedItems);
+    setCurrentPage(1)
   }
+  //pagination logic
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItem = filteredItems.slice(indexOfFirstItem, indexOfLastItem)
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
   return (
     <div>
       {/* Menu bannar */}
@@ -83,51 +94,90 @@ function Menu() {
       {/* menu  shop section */}
       <div className="section-container">
         {/* filtering and Sorting */}
-        <div className="flex flex-row  items-start md:items-center my-2 mx-3 py-2 md:gap-8 gap-4 flex-wrap">
+        <div className="flex flex-col md:flex-row flex-wrap md:justify-between mb-8 items-center space-y-3">
           {/* all category btn  */}
-          <button
-            onClick={showAllMenu}
-            className={selectedCategory === "all" ? "active" : ""}
-          >
-            All
-          </button>
-          <button
-            onClick={() => filterItems("salad")}
-            className={selectedCategory === "salad" ? "active" : ""}
-          >
-            Salad
-          </button>
-          <button
-            onClick={() => filterItems("pizza")}
-            className={selectedCategory === "pizza" ? "active" : ""}
-          >
-            Pizza
-          </button>
-          <button
-            onClick={() => filterItems("soup")}
-            className={selectedCategory === "soup" ? "active" : ""}
-          >
-            Soups
-          </button>
-          <button
-            onClick={() => filterItems("dessert")}
-            className={selectedCategory === "dessert" ? "active" : ""}
-          >
-            Desserts
-          </button>
-          <button
-            onClick={() => filterItems("drinks")}
-            className={selectedCategory === "drinks" ? "active" : ""}
-          >
-            Drinks
-          </button>
+          <div className="flex flex-row  items-start md:items-center  md:gap-8 gap-4 flex-wrap">
+            <button
+              onClick={showAllMenu}
+              className={selectedCategory === "all" ? "active" : ""}
+            >
+              All
+            </button>
+            <button
+              onClick={() => filterItems("salad")}
+              className={selectedCategory === "salad" ? "active" : ""}
+            >
+              Salad
+            </button>
+            <button
+              onClick={() => filterItems("pizza")}
+              className={selectedCategory === "pizza" ? "active" : ""}
+            >
+              Pizza
+            </button>
+            <button
+              onClick={() => filterItems("soup")}
+              className={selectedCategory === "soup" ? "active" : ""}
+            >
+              Soups
+            </button>
+            <button
+              onClick={() => filterItems("dessert")}
+              className={selectedCategory === "dessert" ? "active" : ""}
+            >
+              Desserts
+            </button>
+            <button
+              onClick={() => filterItems("drinks")}
+              className={selectedCategory === "drinks" ? "active" : ""}
+            >
+              Drinks
+            </button>
+          </div>
+          {/* Sorting */}
+          <div className="flex justify-end">
+            <div className="bg-black p-2">
+              <FaFilter className="h-4 w-4 text-white" />
+            </div>
+            {/* sorting options */}
+            <select
+              name="sort"
+              id="sort"
+              onChange={(e) => handleSorting(e.target.value)}
+              value={sortOption}
+              className="bg-black text-white px-1 text-center text-sm  rounded-sm"
+            >
+              <option value="default">Default</option>
+              <option value="A-Z">A - Z</option>
+              <option value="Z-A">Z - A</option>
+              <option value="low-to-high">Low - High</option>
+              <option value="high-to-low">High - High</option>
+            </select>
+          </div>
         </div>
         {/* product cards */}
         <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
-          {filteredItems.map((item, index) => (
+          {currentItem.map((item, index) => (
             <Cards key={item._id} item={item} />
           ))}
         </div>
+      </div>
+
+      {/* paginaton  */}
+      <div className="flex justify-center my-8">
+        {Array.from({
+          length: Math.ceil(filteredItems.length / itemsPerPage),
+        }).map((_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+            className={`mx-1 px-3 py-1 rounded-full ${
+              currentPage === index + 1 ? "bg-green text-white" : "bg-gray-400"
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
       </div>
     </div>
   );
